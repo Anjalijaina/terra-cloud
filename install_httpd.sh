77e535f0e096595639a1f4deb08c9778594c7ed8
Anjalijaina <146342696+Anjalijaina@users.noreply.github.com>
2023-12-20 21:01:20 +0530

    Update install_httpd.sh

--- install_httpd.sh
+++ install_httpd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-yum update -y
-yum install -y httpd
+apt-get update -y
+apt-get install -y httpd
 systemctl start httpd
 systemctl enable httpd
 echo "Hello World from $(hostname -f)" > /var/www/html/index.html
